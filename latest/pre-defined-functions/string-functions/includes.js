f996467e09e6e01e0e33bb2514723c4477c9e5d1
Salman Rasheed <sallu2004mkt@gmail.com>
2025-09-08 10:52:06 +0530

    m

---
+++ latest/pre-defined-functions/string-functions/includes.js
@@ -0,0 +1,6 @@
+// This function checks if a string includes a specific substring.
+
+const str = "Hello, world!";
+const substring = "world";
+const includesSubstring = str.includes(substring);
+console.log(includesSubstring); // Output: true
---
+++ latest/pre-defined-functions/string-functions/slice.js
@@ -0,0 +1,5 @@
+// This function takes a string and returns a sliced version of it.
+
+const str = "Hello, world!";
+const slicedStr = str.slice(0, 5);
+console.log(slicedStr); // Output: "Hello"
