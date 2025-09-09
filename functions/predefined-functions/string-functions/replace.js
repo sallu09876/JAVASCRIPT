ed776f2299cc54ebd973bdb0be255feb482ae9a5
Salman Rasheed <sallu2004mkt@gmail.com>
2025-09-09 10:51:44 +0530

    d

--- functions/predefined-functions/string-functions/replace.js
+++ functions/predefined-functions/string-functions/replace.js
@@ -1,3 +1,7 @@
+// The replace() method replaces a specified value with another value in a string.
+
+// This method does not change the original string. It returns a new string.
+
 let str = `hello world`;
 let strReplace = str.replace(`world`, `boys`);
 console.log(str);
